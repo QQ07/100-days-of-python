610beff9837375647588c0bc642fd6b1547e1d4b
vrohan <rohan.22110021@viit.ac.in>
2023-06-26 18:03:09 +0530

    Completed day 8 Caesar Cipher

--- day8.py
+++ day8.py
@@ -1,19 +1,38 @@
+"""encrypt decrypt"""
+
+
 def main():
+    """Main Function"""
     print("Welcome to Caesar Cipher")
     print("Press 1 to Encrypt\nPress 2 to Decrypt")
-    user_input =int(input())
-    if user_input==1:
+    user_input = int(input())
+    if user_input == 1:
         encrypt()
-    if user_input==2:
+    if user_input == 2:
         decrypt()
-      
+
 
 def encrypt():
-    Ostring = input("Enter the original string: ")
-    for i in range(len(Ostring)):
-        
+    """A function that Encrypts"""
+    original_string = input("Enter the original string: ")
+    ans = ""
+    for i in original_string:
+        ans += chr(ord(i) + 5)
+    print(F"Encrypted String: {ans}")
 
 
 def decrypt():
-    Estring = input("Enter the Encrypted string: ")
-    
+    """A function that Decrypts"""
+    encrypted_string = input("Enter the Encrypted string: ")
+    ans = ""
+    for i in encrypted_string:
+        ans += chr(ord(i) - 5)
+    print(f"Decrypted String: {ans}")
+
+
+TO_CONTINUE = True
+while TO_CONTINUE:
+    main()
+    u_i = input("Continue? ").lower()
+    if u_i == "no":
+        TO_CONTINUE = False
